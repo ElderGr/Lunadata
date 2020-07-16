a5c65ea627846342d69ac2a5b9219ba0ed951ea2
ElderGr <gbielrs@gmail.com>
2020-07-16 13:44:35 -0300

    Updates

---
+++ precache-manifest.c4feb29fcf035edad01ef034f6d5e37f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e5fb7e4c4bf0d96693963c9c6de6ac",
+    "url": "/Lunadata/index.html"
+  },
+  {
+    "revision": "e422e12cfc1810db7a66",
+    "url": "/Lunadata/static/css/main.27b60e2a.chunk.css"
+  },
+  {
+    "revision": "b786416adef09738afff",
+    "url": "/Lunadata/static/js/2.b8d211a0.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/Lunadata/static/js/2.b8d211a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e422e12cfc1810db7a66",
+    "url": "/Lunadata/static/js/main.a48e42f1.chunk.js"
+  },
+  {
+    "revision": "7344c49be0dd1524e617",
+    "url": "/Lunadata/static/js/runtime-main.f7180eb8.js"
+  },
+  {
+    "revision": "9fedc502b9e8e46b2d1416006ea77fb8",
+    "url": "/Lunadata/static/media/galaxy.9fedc502.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e2e7674cf1053bf6f1876997fb94f60.js"
+  "/Lunadata/precache-manifest.c4feb29fcf035edad01ef034f6d5e37f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Lunadata/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
